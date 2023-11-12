4c18a557b084849dbffd257e6ef06e8f9b35c285
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2023-11-12 15:27:48 -0300

    first commit

---
+++ ComposableArchitecture-Sample/ComposableArchitecture_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitecture_SampleApp.swift
+//  ComposableArchitecture-Sample
+//
+//  Created by Tiago Oliveira on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitecture_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
