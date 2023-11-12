27fb64df9534d72aa4731cb9a26844ed907a2787
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2023-11-12 17:37:10 -0300

    [Section 3] Integrating into the app

--- ComposableArchitecture-Sample/ComposableArchitecture_SampleApp.swift
+++ ComposableArchitecture-Sample/ComposableArchitecture_SampleApp.swift
@@ -5,13 +5,22 @@
 //  Created by Tiago Oliveira on 12/11/23.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct ComposableArchitecture_SampleApp: App {
+    // MARK: It is important to note that the Store that powers the application should only be created a single time. For most applications it should be sufficient to create it directly in the WindowGroup at the root of the scene. But, it can also be held as a static variable and then provided in the scene.
+    
+    // TIP: `_printChanges` it will print every action that the reducer processes to the console, and it will print how the state changed after processing the action.
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterFeatureView(store: ComposableArchitecture_SampleApp.store)
         }
     }
 }
